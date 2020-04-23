9caa3c21aaf49c7c615aa708e713d8389835b846
Pierre antoine <pierre.antoine@epitech.eu>
2020-04-23 08:53:09 +0200

    reset

--- ImgurAppClone/env/Environement.js
+++
@@ -1,7 +0,0 @@
-
-const environement = {
-    Client_ID: '6abfd5936448c09',
-    Base_Auth: 'https://api.imgur.com/oauth2/authorize?client_id=6abfd5936448c09&response_type=token'
-};
-
-export default environement;
