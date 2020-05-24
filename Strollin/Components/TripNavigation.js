50d51bf4116ebdce488ea26dc0e9beed7234ecc2
Pierre antoine <pierre.antoine@epitech.eu>
2020-05-24 18:57:32 +0200

    add waypoints to maps

--- Strollin/Components/TripNavigation.js
+++ Strollin/Components/TripNavigation.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { Component , useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Button, Image} from 'react-native';
-//import stylesGeneric from '../../styles/genericStyle';
+
+import {connect} from 'react-redux';
 
 import Map from './map';
 
-export default class TripNavigation extends React.Component {
+function TripNavigation(props) {
+
+  const region = {
+      latitude: 48.815641,
+      longitude: 2.363224,
+      name: '',
+  }
+
+  const waypoints = [
+    {
+      latitude: 47.815641,
+      longitude: 2.363224,
+      name: '1',
+    },{
+      latitude: 46.815641,
+      longitude: 2.363224,
+      name: '2',
+    },{
+      latitude: 45.815641,
+      longitude: 2.363224,
+      name: '3',
+    },{
+      latitude: 44.815641,
+      longitude: 2.363224,
+      name: '4',
+    },
+  ]
+
+  const deltaView = {
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={{flex: 0.3, marginTop: 40}}>
+        <Text style={{fontSize: 30, fontFamily:"lobster", color:"#EEB015"}}>Strollin</Text>
+      </View>
+      <View style={{flex: 3}}>
+        <Map location={region} height={"100%"} width={380} markers={waypoints} deltaView={deltaView} waypoints={waypoints} />
+      </View>
+      <View style={{flex: 1, position: 'absolute', bottom: 0, left: 0, marginTop: 10}}>
+        <Button
+          title="End Navigation"
+          onPress={() => props.navigation.navigate('Home')}
+        />
+      </View>
+    </View>
+  );
+
+}
+
+const mapStateToProps = (state) => {
+  return state
+}
+export default connect(mapStateToProps)(TripNavigation);
+
+
+export class TripNavigation2 extends React.Component {
   render() {
     //console.disableYellowBox = true;
 
@@ -22,11 +81,6 @@ export default class TripNavigation extends React.Component {
         title: "USER",
         subtitle: "User position"
       },
-      count: 0,
-      needCoords: true,
-      permission: false,
-      needTrip: true,
-      ready: false
     }
 
     const { navigation } = this.props;
--- Strollin/Components/map.js
+++ Strollin/Components/map.js
@@ -92,14 +92,15 @@ function Map(props) {
               region={{
                 latitude: props.position.position.latitude,
                 longitude: props.position.position.longitude,
-                latitudeDelta: props.location.latitudeDelta,
-                longitudeDelta: props.location.longitudeDelta
+                latitudeDelta: props.deltaView.latitudeDelta,
+                longitudeDelta: props.deltaView.longitudeDelta
               }}
               onRegionChange={onRegionChange}
               >
               <MapViewDirections
                 origin={origin}
                 destination={destination}
+                waypoints = {props.waypoints}
                 apikey={GOOGLE_MAPS_APIKEY}
                 strokeWidth={7}
                 strokeColor="#39A5D6"
@@ -131,134 +132,4 @@ function Map(props) {
 const mapStateToProps = (state) => {
   return state
 }
-
-//connect(mapStateToProps)(requestGeolocalisationPermission);
-//connect(mapStateToProps)(updatePosition);
-
 export default connect(mapStateToProps)(Map);
-
-class Map2 extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      permission: false,
-      user: {
-        latitude: 48.815641,
-        longitude: 2.363224,
-        title: "USER",
-        subtitle: "User position"
-      }
-    }
-    this.updateCoordinates();
-  };
-
-  componentDidMount() {
-    this.requestGeolocalisationPermission();
-    this.updateCoordinates();
-    console.log(this.state.permission);
-  }
-
-  updatePosition(position) {
-    //console.log(position);
-    let newLatitude = position.coords.latitude;
-    let newLongitude = position.coords.longitude;
-
-    this.setState({
-      user: {
-        latitude: newLatitude,
-        longitude: newLongitude,
-        title: "USER",
-        subtitle: "User position"
-      }
-    })
-  }
-
-  updateCoordinates = () => {
-    if (this.state.permission == false) {
-      Geolocation.getCurrentPosition(
-        this.updatePosition.bind(this),
-        (error) => {
-          console.log(error.code, error.message);
-        },
-        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-      );
-    }
-  };
-
-  async requestGeolocalisationPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          title: 'Strollin ask geolocalisation Permission',
-          message: '',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        this.state.permission = true;
-        this.setState({ permission: true })
-        console.log('You can use the geolocalisation');
-      } else {
-        console.log('geolocalisation permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
-
-  onRegionChange(region) {
-    //console.log(region);
-
-    //this.setState({ region });
-  }
-
-  render() {
-    const origin = { latitude: this.state.user.latitude, longitude: this.state.user.longitude };
-    const destination = { latitude: this.props.location.latitude, longitude: this.props.location.longitude };
-    const GOOGLE_MAPS_APIKEY = 'AIzaSyB-xO-UPVjhXP85uteh1n5hIbUeXqqjWRI';
-    if (this.state.permission) {
-      return (
-        <MapView style={{ height: this.props.height, width: this.props.width }}
-          region={{
-            latitude: this.state.user.latitude,
-            longitude: this.state.user.longitude,
-            latitudeDelta: this.props.location.latitudeDelta,
-            longitudeDelta: this.props.location.longitudeDelta
-          }}
-          onRegionChange={this.onRegionChange}
-        >
-          <MapViewDirections
-            origin={origin}
-            destination={destination}
-            apikey={GOOGLE_MAPS_APIKEY}
-            strokeWidth={7}
-            strokeColor="#39A5D6"
-          />
-          <Marker
-            coordinate={{ "latitude": this.state.user.latitude, "longitude": this.state.user.longitude }}
-            title={"USER"}
-            description={"User position"}
-          />
-          {this.props.markers.map(marker => (
-            <Marker
-              coordinate={{ "latitude": marker.latitude, "longitude": marker.longitude }}
-              title={marker.name}
-              description={"Destination"}
-            />
-          ))}
-
-
-        </MapView>
-      );
-    } else {
-      return (
-        <View>
-          <Text>No permision</Text>
-        </View>
-      )
-    }
-  }
-}
