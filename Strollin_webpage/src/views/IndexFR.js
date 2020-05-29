dbf70eff9cf83ac89122e6179cd22175328526f8
Tony Ye <tony.ye@epitech.eu>
2020-05-29 20:39:34 +0200

    [DEL] deleting old files

--- Strollin_webpage/src/views/IndexFR.js
+++ Strollin_webpage/src/views/IndexFR.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-import IndexNavbar from "components/Navbars/IndexNavbar.js";
-import IndexHeader from "components/Headers/IndexHeader.js";
 import IndexNavbarFR from "components/Navbars/IndexNavbarFR.js";
 import IndexHeaderFR from "components/Headers/IndexHeaderFR.js";
-import DemoFooter from "components/Footers/DemoFooter.js";
 
 import ContactFR from "views/index-sections/ContactFR.js";
 import TemoignageFR from "views/index-sections/TemoignageFR.js";
@@ -13,18 +10,6 @@ import FaqFR from "views/index-sections/FaqFR.js";
 import SectionQuestionFR from "views/index-sections/SectionQuestionFR.js";
 
 import DescriptionFR from "views/index-sections/DescriptionFR.js";
-import SectionNavbars from "views/index-sections/SectionNavbars.js";
-import SectionNavigation from "views/index-sections/SectionNavigation.js";
-import SectionProgress from "views/index-sections/SectionProgress.js";
-import SectionNotifications from "views/index-sections/SectionNotifications.js";
-import SectionTypography from "views/index-sections/SectionTypography.js";
-import SectionJavaScript from "views/index-sections/SectionJavaScript.js";
-import SectionCarousel from "views/index-sections/SectionCarousel.js";
-import SectionNucleoIcons from "views/index-sections/SectionNucleoIcons.js";
-import SectionDark from "views/index-sections/SectionDark.js";
-import SectionLogin from "views/index-sections/SectionLogin.js";
-import SectionExamples from "views/index-sections/SectionExamples.js";
-import SectionDownload from "views/index-sections/SectionDownload.js";
 
 function IndexFR() {
   document.documentElement.classList.remove("nav-open");
