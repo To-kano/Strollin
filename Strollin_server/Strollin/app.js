2dee56b43230cf7ca2be4f2e77d40848f8701cf7
Thomas <thomas1.vigier@epitech.eu>
2020-10-03 16:23:22 +0200

    Merging algo branch

--- Strollin_server/Strollin/app.js
+++ Strollin_server/Strollin/app.js
@@ -20,16 +20,15 @@ var app = express();
 
 // MongoDB //
 
-//var mongoDB = 'mongodb://db:27017/Strollin';
-//mongoose.connect(mongoDB, { useNewUrlParser: true });
+var mongoDB = 'mongodb://db:27017/Strollin';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 //Get the default connection
-//var db = mongoose.connection;
+var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-//db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-/*******/
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -72,18 +71,18 @@ promise1.then((value) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  //next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
