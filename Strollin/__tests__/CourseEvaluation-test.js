91e2a4c9aa519acb14c8579c63e172b861f4bc91
Pierre antoine <pierre.antoine@epitech.eu>
2021-01-02 15:17:04 +0100

    update test

--- Strollin/__tests__/CourseEvaluation-test.js
+++ Strollin/__tests__/CourseEvaluation-test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import CourseEvaluation from '../Components/CourseEvaluation';
 
+import Store from '../Store/configureStore';
+import { Provider } from 'react-redux';
+
 describe('CourseEvaluation', () => {
     describe('Rendering', () => {
         it('should match to snapshot', () => {
-            const component = shallow(<CourseEvaluation />)
+            const component = shallow(
+            <Provider store={Store}>
+                <CourseEvaluation />
+            </Provider>)
             expect(component).toMatchSnapshot()
         });
     });
