b3df90234a59f994820e0716ec57e085fcff8c12
Thomas <thomas1.vigier@epitech.eu>
2021-01-03 15:06:05 +0100

    pushing before pulling master

--- Strollin/env/Environement.js
+++ Strollin/env/Environement.js
@@ -2,13 +2,13 @@ const environement = {
     Client_ID: '6abfd5936448c09',
     Base_Auth: 'https://api.imgur.com/oauth2/authorize?client_id=6abfd5936448c09&response_type=token'
   };
-  
+
   exports.environement = environement;
-  
-  const IP_SERVER = process.env.IP_SERVER || '82.226.234.122';
-  
+
+  const IP_SERVER = process.env.IP_SERVER || '86.236.229.156';
+
   exports.IP_SERVER = IP_SERVER;
-  
-  const PORT_SERVER = process.env.PORT_SERVER || '3003';//3003 pour Tony
-  
+
+  const PORT_SERVER = process.env.PORT_SERVER || '3000';//3003 pour Tony
+
   exports.PORT_SERVER = PORT_SERVER;
