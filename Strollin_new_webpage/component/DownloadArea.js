b07bf38fb3b6513cfcc8a98069e5877d3d52302a
Basile <basile.samson@epitech.eu>
2021-06-18 20:17:13 +0200

    front new webpage

--- Strollin_new_webpage/component/DownloadArea.js
+++ Strollin_new_webpage/component/DownloadArea.js
@@ -6,7 +6,7 @@ function PartnerArea() {
   
   return (
     <>
-     <div className={styles.row}>
+     <section>
        <div >
           <p>How can we help you in you business?</p>
           <p>Lorem ipsum</p>
@@ -15,7 +15,7 @@ function PartnerArea() {
        <video controls width="300" height="500">
           <source src={"./Strollin_demo.mp4"} type="video/mp4"/>
         </video>
-     </div>
+     </section>
     </>
   );
 }
--- Strollin_new_webpage/component/FeaturesArea.js
+++ Strollin_new_webpage/component/FeaturesArea.js
@@ -6,50 +6,51 @@ function FeaturesArea() {
   
   return (
     <>
-      <div className="container">
-          <div className="row d-flex justify-content-center">
-                <div className="section-tittle text-center">
-                    <span style={{fontSize:"50px", fontWeight:"bold"}}><br/>Qu'est-ce que Strollin'?<br/><br/></span>
-                </div>
+      <section className={styles.center}>
+        <h2>Our features</h2>
+        <div className={styles.table}>
+
+        <div className={styles.box}>
+              <img src={"./icons/map.png"}/>
+              <h2>App de Navigation</h2>
+              <p>Le but de Strollin' est de vous guider grâce une navigation personnalisée. Strollin' vous proposera diverses ensembles de destinations choisi selon votre temps libre, votre budget et vos goûts !</p>
+            </div>
+
+            <div className={styles.box}>
+              <img src={"./icons/chats.png"}/>
+              <h2>Basé sur votre Profil</h2>
+              <p>L'algorithme de Strollin' se base sur les informations que vous lui donnez ! Les tags que vous choisirez permettront à Strollin' d'être de plus en plus précis et de composer les meilleures ensembles de destination pour vous !</p>
+            </div>
+
+            <div className={styles.box}>
+              <img src={"./icons/profile.png"}/>
+              <h2>Totallement gratuit !</h2>
+              <p>Strollin' comportent quelques achats intégrés, mais est surtout totallement gratuit et ne compte pas sur la publicité !</p>
+            </div>
+            
+            <div className={styles.box}>
+              <img src={"./icons/map.png"}/>
+              <h2>App de Navigation</h2>
+              <p>Le but de Strollin' est de vous guider grâce une navigation personnalisée. Strollin' vous proposera diverses ensembles de destinations choisi selon votre temps libre, votre budget et vos goûts !</p>
+            </div>
+
+            <div className={styles.box}>
+              <img src={"./icons/map.png"}/>
+              <h2>Basé sur votre Profil</h2>
+              <p>L'algorithme de Strollin' se base sur les informations que vous lui donnez ! Les tags que vous choisirez permettront à Strollin' d'être de plus en plus précis et de composer les meilleures ensembles de destination pour vous !</p>
+            </div>
+
+            <div className={styles.box}>
+              <img src={"./icons/money-box.png"}/>
+              <h2>Totallement gratuit !</h2>
+              <p>Strollin' comportent quelques achats intégrés, mais est surtout totallement gratuit et ne compte pas sur la publicité !</p>
+            </div>
+
           </div>
-          <br/>
-          <div className="row">
-              <div className="col-xl-4 col-lg-4 col-md-6">
-                  <div className="services-caption text-center mb-30">
-                      <div className={ 'fas fa-hand-holding-usd' }>
-                        <img size="30" src={"./gpsIcon.png"} style={{ width: 75, height: 75}}/>,
-                        <h4 style={{fontSize:"25px", fontWeight:"bold"}}>App de Navigation<br/></h4>
-                        <p style={{fontSize:"18px", fontWeight:"500"}}><br/>Le but de Strollin' est de vous guider grâce une navigation personnalisée. Strollin' vous proposera diverses ensembles de destinations choisi selon votre temps libre, votre budget et vos goûts !</p>
-                      </div>
-                  </div>
-              </div>
-              <div className="col-xl-4 col-lg-4 col-md-6">
-                  <div className="services-caption active text-center mb-30">
-                      <div className="service-icon">
-                          <span className="flaticon-pay"></span>
-                      </div> 
-                      <div className="service-cap">
-                        <img size="30" src={"./profileIcon.png"} style={{ width: 75, height: 75}}/>,
-                          <h4 style={{fontSize:"25px", fontWeight:"bold"}}>Basé sur votre Profil<br/></h4>
-                          <p style={{fontSize:"18px", fontWeight:"500"}}><br/>L'algorithme de Strollin' se base sur les informations que vous lui donnez ! Les tags que vous choisirez permettront à Strollin' d'être de plus en plus précis et de composer les meilleures ensembles de destination pour vous !</p>
-                      </div>
-                  </div>
-              </div> 
-              <div className="col-xl-4 col-lg-4 col-md-6">
-                  <div className="services-caption text-center mb-30">
-                      <div className="service-icon">
-                          <span className="flaticon-plane"></span>
-                      </div> 
-                      <div className="service-cap">
-                        <img size="30" src={"./freeIcon.png"} style={{ width: 75, height: 75}}/>,
-                          <h4 style={{fontSize:"25px", fontWeight:"bold"}}>Totallement gratuit !</h4>
-                          <p style={{fontSize:"18px", fontWeight:"500"}}><br/>Strollin' comportent quelques achats intégrés, mais est surtout totallement gratuit et ne compte pas sur la publicité !</p>
-                      </div>
-                  </div>
-              </div>
+          <div>
+            <button>Learn More</button>
           </div>
-          <br/><br/><hr/><br/>
-      </div>
+      </section>
     </>
   );
 }
