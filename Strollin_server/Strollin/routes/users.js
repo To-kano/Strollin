2392ea9833b5089e4bb8a31c2981df2d500631dc
Cheng Didier <Vangnex@localhost.localdomain>
2021-08-17 18:33:02 +0200

    UPDATE Readme and login_web crypt

--- Strollin_server/Strollin/routes/users.js
+++ Strollin_server/Strollin/routes/users.js
@@ -610,7 +610,7 @@ router.get('/login', async function (req, res) {
  */
  router.post('/login_web', async function (req, res) {
 
-  let user = await UserModel.findOne({ mail: req.body.mail, password: req.body.password }).catch(error => error);
+  let user = await UserModel.findOne({ mail: req.body.mail.toLowerCase(), password: CryptoJS.HmacSHA1(req.body.password, keyCrypto).toString() }).catch(error => error);
   let token = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   let error = undefined;
   let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
