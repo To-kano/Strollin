c1635a95ce1b38e4260c03175b6ba1de5b45ebc7
Cheng Didier <Vangnex@localhost.localdomain>
2021-08-27 16:17:27 +0200

    ADD set subscription route

--- Strollin_server/Strollin/routes/subscription.js
+++ Strollin_server/Strollin/routes/subscription.js
@@ -4,6 +4,10 @@ var router = express.Router();
 const stripe = require('stripe')('sk_test_51JSMW9H9C7g7Ir78BQDjbCPJ71SYc19nSmTDAEZAqrMHcREIMi6SOhHaEuGspN62eT3g5Iza1QKex8ifc0a2jKGn00wrObLsAs')
 var customer = undefined;
 
+const {
+    UserModel
+  } = require("../models/user")
+
 // Manage the customer with stripe.customers
 // Retrieve = stripe.customers.retrieve(stripeID)
 // Create = stripe.customers.create() Pas de parametre pour le moment car non nécessaire pour créer un client sur stripe
@@ -30,5 +34,26 @@ router.post('/payment_intent', async function(req, res) {
     return res.status(200).send({status: "Payment created", customer});
 });
 
+router.get('/set_subscription', async function(req, res) {
+    let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id").catch(error => error);
+
+    if (!user) {
+        return res.status(400).send({ status: "You are not connected." });
+    }
+    if (user.reason) {
+        return res.status(400).send({ status: "Error in database transaction:\n", error: user });
+    }
+    if (user.stripe_id === '') {
+        stripe.customers.create({
+            email: user.mail,
+
+        })
+    }
+
+    customer = await stripe.customers.retrieve(user.stripe_id)
+    .catch(error => {
+        return res.status(400).send({status: "An error occurred during the subscription setting"})
+    });
+});
 
 module.exports = router;
