779a46df22688b698f4cc6f14c3d324195889ae3
Cheng Didier <Vangnex@localhost.localdomain>
2021-08-27 17:56:01 +0200

    UPDATE Create_customer route created

--- Strollin_server/Strollin/routes/subscription.js
+++ Strollin_server/Strollin/routes/subscription.js
@@ -9,8 +9,13 @@ const {
   } = require("../models/user")
 
 
-router.get('/set_subscription', async function(req, res) {
-    let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id").catch(error => error);
+// CREATE_CUSTOMER
+/**
+ * Create or retrieve a customer for Stripe
+ * @param {String} req.headers.access_token
+ */
+router.post('/create_customer', async function(req, res) {
+    let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id mail stripe_id").catch(error => error);
 
     if (!user) {
         return res.status(400).send({ status: "You are not connected." });
@@ -19,16 +24,42 @@ router.get('/set_subscription', async function(req, res) {
         return res.status(400).send({ status: "Error in database transaction:\n", error: user });
     }
     if (user.stripe_id === '') {
-        stripe.customers.create({
+        customer = await stripe.customers.create({
             email: user.mail,
-            
         })
+        .catch(error => {
+            return res.status(400).send({status: "An error occurred during the creation of customer"})
+        });
+        let error = await UserModel.updateOne({ id: user.id }, {stripe_id: customer.id}).catch(error => error);
+        if (error.errors) {
+            return res.status(400).send({ status: "Error in database transaction:\n", error: error.errors });
+        }
+    } else {
+        customer = await stripe.customers.retrieve(user.stripe_id)
+        .catch(error => {
+            return res.status(400).send({status: "An error occurred during the retrieve of customer"})
+        });
     }
-
-    customer = await stripe.customers.retrieve(user.stripe_id)
-    .catch(error => {
-        return res.status(400).send({status: "An error occurred during the subscription setting"})
-    });
+    return res.status(200).send({status: "The customer is created/retrieved", id: customer.id});
 });
+
+
+// STOP_SUBSCRIPTION
+/**
+ * Stop the subscription of an user
+ * @param {String} req.headers.access_token
+ */
+ router.post('/stop_subscription', async function(req, res) {
+    let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id").catch(error => error);
+
+    if (!user) {
+        return res.status(400).send({ status: "You are not connected." });
+    }
+    if (user.reason) {
+        return res.status(400).send({ status: "Error in database transaction:\n", error: user });
+    }
+    //...
+});
+
 
 module.exports = router;
