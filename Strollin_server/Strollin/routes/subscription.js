b3b4444fd2e387c7139fe084a6426bf971bd63e2
Cheng Didier <Vangnex@localhost.localdomain>
2021-11-12 17:32:26 +0100

    UPDATE Subcription and webhook for stripe works, just need to set function when event come

--- Strollin_server/Strollin/routes/subscription.js
+++ Strollin_server/Strollin/routes/subscription.js
@@ -6,7 +6,35 @@ var customer = undefined;
 
 const {
     UserModel
-  } = require("../models/user")
+} = require("../models/user")
+
+
+// CREATE_SESSION
+/**
+ * Create a session for Stripe
+ * @param {String} req.headers.access_token
+**/
+router.post('/create_session', async function(req, res) {
+    const price_id = "price_1JSl53H9C7g7Ir7890KpWvW8";
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card', 'sepa_debit'],
+
+        line_items: [{
+            price: price_id,
+            quantity: 1,
+        }],
+        mode: 'subscription',
+        customer: 'cus_K6zMdyiRx5gSbG',
+        success_url: 'https://stripe.com/docs/billing/subscriptions/build-subscription',
+        cancel_url: 'https://stripe.com/docs/billing/subscriptions/sepa-debit',
+    });
+    if (session) {
+        return res.status(200).send({ status: "Session created", session});
+    }
+    return res.status(400).send({ status: "Session failed"});
+});
+
 
 
 // CREATE_CUSTOMER
@@ -50,41 +78,41 @@ router.post('/create_customer', async function(req, res) {
  * Create an incomplete subscription ready to be confirmed by getting payment confirmation.
  * 
  */
-router.post('/create_subscription', async function(req, res) {
-    const price_id = "price_1JSl53H9C7g7Ir7890KpWvW8";
-    var subscription = undefined;
+// router.post('/create_subscription', async function(req, res) {
+//     const price_id = "price_1JSl53H9C7g7Ir7890KpWvW8";
+//     var subscription = undefined;
 
-    let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id stripe_id").catch(error => error);
+//     let user = await UserModel.findOne({ access_token: req.headers.access_token }, "-_id id stripe_id").catch(error => error);
 
-    if (!user) {
-        return res.status(401).send({ error_code: 1 });
-    }
-    if (user.reason) {
-        return res.status(500).send({ error_code: 2 });
-    }
-    if (user.stripe_id !== '') {
+//     if (!user) {
+//         return res.status(401).send({ error_code: 1 });
+//     }
+//     if (user.reason) {
+//         return res.status(500).send({ error_code: 2 });
+//     }
+//     if (user.stripe_id !== '') {
 
-        subscription = await stripe.subscriptions.create({
-            customer: user.stripe_id,
-            items: [{
-                price: price_id,
-            }],
-            payment_behavior: 'default_incomplete',
-            expand: ['latest_invoice.payment_intent'],
-        })
-        .catch(error => {
-            return res.status(500).send({ error_code: 2 });
-        });
-    } else {
-        return res.status(500).send({ error_code: 2 });
-    }
+//         subscription = await stripe.subscriptions.create({
+//             customer: user.stripe_id,
+//             items: [{
+//                 price: price_id,
+//             }],
+//             payment_behavior: 'default_incomplete',
+//             expand: ['latest_invoice.payment_intent'],
+//         })
+//         .catch(error => {
+//             return res.status(500).send({ error_code: 2 });
+//         });
+//     } else {
+//         return res.status(500).send({ error_code: 2 });
+//     }
 
-    let error = await UserModel.updateOne({ id: user.id }, {subscription_id: subscription.id}).catch(error => error);
-    if (error.errors) {
-        return res.status(500).send({ error_code: 2 });
-    }
-    return res.status(200).send({ status: "The subscription is created, waiting to confirm the payment.", subscription_id: subscription.id, client_secret: subscription.latest_invoice.payment_intent.client_secret});
-});
+//     let error = await UserModel.updateOne({ id: user.id }, {subscription_id: subscription.id}).catch(error => error);
+//     if (error.errors) {
+//         return res.status(500).send({ error_code: 2 });
+//     }
+//     return res.status(200).send({ status: "The subscription is created, waiting to confirm the payment.", subscription_id: subscription.id, client_secret: subscription.latest_invoice.payment_intent.client_secret});
+// });
 
 
 // GET_SUBSCRIPTION
@@ -136,47 +164,40 @@ router.post('/stop_subscription', async function(req, res) {
  * WEBHOOK
  **/
 
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = "whsec_4foNz7yskrBbBlsUdus1WtPOMJE7p2Tj";
-
 router.post('/webhook', async function(req, res) {
-    const sig = request.headers['stripe-signature'];
-
-    let event;
-
-    try {
-        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err) {
-        response.status(400).send(`Webhook Error: ${err.message}`);
-        return;
-    }
+    let event = req.body;
 
     // Handle the event
     switch (event.type) {
         case 'checkout.session.completed':
-            const session = event.data.object;
+            console.log("session completed:\n");
+            // console.log(event.data.object);
             // Then define and call a function to handle the event checkout.session.completed
             break;
         case 'customer.subscription.deleted':
-            const subscription = event.data.object;
+            console.log("Sub deleted:\n");
+            // console.log(event.data.object);
             // Then define and call a function to handle the event customer.subscription.deleted
             break;
         case 'invoice.paid':
-            const invoice = event.data.object;
+            console.log("invoice paid:\n");
+            // console.log(event.data.object);
             // Then define and call a function to handle the event invoice.paid
             break;
         case 'invoice.payment_failed':
-            const invoice = event.data.object;
+            console.log("invoice failure:\n");
+            // console.log(event.data.object);
             // Then define and call a function to handle the event invoice.payment_failed
             break;
     // ... handle other event types
     default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
+        console.log(`Unhandled event type ${event.type}`);
+    }
 
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
+    // Return a 200 response to acknowledge receipt of the event
+    res.send();
 });
+
 
 
 module.exports = router;
